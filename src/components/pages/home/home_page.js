75daf0a897a4a287a5f8e072e7743ad4a7e1fb1d
Chris <ctomotsugu@sapient.com>
2017-10-04 11:25:44 -0400

    updated files

--- src/components/pages/home/home_page.js
+++ src/components/pages/home/home_page.js
@@ -5,8 +5,11 @@ import { Link } from 'react-router-dom';
 import { fetchProfileImages } from '../../../actions';
 import styles from '../../../style/styles.css';
 
+import Example from './home_page_card';
+
 import { Container, Row, Col } from 'reactstrap';
-import { Nav, NavItem, NavLink } from 'reactstrap';
+import { Nav, NavItem, NavLink, Button } from 'reactstrap';
+import { CardBody, Card, CardImg, CardTitle, CardText, CardDeck, CardSubtitle } from 'reactstrap';
 
 class HomePage extends Component {
 	componentDidMount(){
@@ -30,17 +33,40 @@ class HomePage extends Component {
 		})
 	}
 
+	// renderProfileCard(){
+
+	// 	return _.map(this.props.profileimages, profimg2 => {
+
+	// 		return (
+	// 			<Card>
+	// 				<CardImg top width="100%" key={profimg2.id} src={ profimg2.id }/>
+	// 				<CardBody>
+	// 					<CardTitle>Card Title</CardTitle>
+	// 					<CardSubtitle>Card subtitle</CardSubtitle>
+	// 					<CardText>This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</CardText>
+	// 					<Button>Button</Button>
+	// 				</CardBody>
+	// 			</Card>
+	// 		);
+	// 	})
+	// }
+
 	render(){
 		
 		return (
 			<div className={ styles.homeProfileMenu }>
-				<Row>
-					<Col sm="6">
-						<Row>
-							{this.renderProfileThumbs()}
-						</Row>
-					</Col>
-				</Row>
+				<Col sm="6">
+						{this.renderProfileThumbs()}
+				</Col>
+				<Card className={ styles.homeProfileMenuBG }>
+					        <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180" alt="Card image cap" />
+					        <div className={ styles.homeProfileMenuCARDBODY }>
+					          <CardTitle>Card title</CardTitle>
+					          <CardSubtitle>Card subtitle</CardSubtitle>
+					          <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
+					          <Button>Button</Button>
+					        </div>
+					      </Card>
 			</div>
 		);
 	}
